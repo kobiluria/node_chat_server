3d6eb7094ce776555fe420df4dfc82ef5bc2b39f
Rami <aritov@gmail.com>
2014-03-11 16:05:31 +0200

    server changes

--- node_packages/server/server.js
+++ node_packages/server/server.js
@@ -33,22 +33,68 @@ console.log("Server listening on port : " + appPort);
 
 // io socket connection :
 io.sockets.on('connection', function (socket) { // First connection
-        var chatrooms = [];
-        userid = 0;
+    var chatrooms = [];
+    var userid = 0;
+    var username = 0;
 
+    io.sockets.socket(socket.id).emit("connected", {});
         socket.on('join' , function(msg) {
             var chatid = msg.chatid;
-            pool.query('select pkey from chattoken where userid = ? and chatid = ? and tokenid = ?',
+            pool.query('select pkey from chattoken where userid = ?? and chatid = ?? and tokenid = ??',
                 [userid, msg.chatid,msg.tokenid], function(err,results) {
                     if (!err &&  results.length ) {
                         if (!chatrooms.contains(chatid)) chatrooms.push(chatid);
                         pool.query('delete from chattoken where tokenid = ?', [msg.tokenid]);
                         socket.join('c' + chatid);
+                        io.sockets.socket(socket.id).emit("inroom", {userid: userid, chatid: chatid});
+
                     }
                 });
         });
-        socket.on('identify', function(msg) {
-           userid = msg.id;
+
+        socket.on('addchat', function(msg) {
+            var chatid = msg.chatid;
+            var chattext = msg.chattext;
+            var chatnum  = msg.chatnum;
+            var medianum = msg.medianum;
+
+            var chatkey = 0;
+            if (chatrooms.contains(chatid)) {
+                async.series([
+                    function(callback) {
+                        pool.query('select chatnum from chats where chatnum = ??',chatnum, function(err,results) {
+                            if (err) {
+                                callback(err);
+                            }
+                            else {
+                                if (results.length) {
+                                    io.sockets.socket(socket.id).emit("chatnum-exists", {chatnum: chatnum});
+                                }
+                            }
+                        })
+                    },
+                    function(callback) {
+                        var time = Math.floor(new Date().getTime() / 1000);
+                        pool.query('insert into chats (chatid,chattime,userid,chattext, chatnum,medianum) values (??,??,??,??,??,??)',
+                            [chatid,time,userid,chattext,chatnum,medianum],
+                            function(err,result) {
+                                if (err || !result || !result.inserId) {
+                                    callback(err);
+                                }
+                                else {
+                                    chatkey = result.insertId;
+                                    var mediaurl = '{chatid}.{userid}.{medianum}'.substitute({chatid: chatid,userid: userid,medianum: medianum});
+                                    var chatinfo = [msg.fullname,msg.userpic,chattext,time,chatkey,chatnum,mediaurl];
+                                    io.sockets.socket(socket.id).emit("chatnum-exists", {chatnum: chatnum});
+                                    socket.broadcast.to('c' + chatid).emit('add-chat', {chatinfo: chatinfo});
+                                }
+                            });
+                    }
+                ],
+                    function(err) {
+                        console.log(err);
+                    });
+            }
         });
 
         socket.on('disconnect', function () { // Disconnection of the client
@@ -59,6 +105,3 @@ io.sockets.on('connection', function (socket) { // First connection
     }
 );
 
-function send_user_count() { // Send the count of the users to all
-    io.sockets.emit('num_of_users', {"num_of_users": users});
-}
